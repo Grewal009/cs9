2e70dabc2b7bb03e12eacd94bb7d23396aaf27f2
Aneel Grewal <aneel.grewal@hotmail.com>
2024-06-03 23:01:34 +0200

    Person class created

---
+++ ConsoleApp20/Person.cs
@@ -0,0 +1,23 @@
+namespace ConsoleApp20;
+
+public class Person
+{
+    public string Name { get; private set; }
+    public int NumberOfKidney { get; private set; }
+    public string HealthStatus { get; private set; }
+    
+    public Person(string name, int numberOfKidney, string healthStatus)
+    {
+        Name = name;
+        NumberOfKidney = numberOfKidney;
+        HealthStatus = healthStatus;
+    }
+
+    public void ShowStatus()
+    {
+        Console.WriteLine($"Name:{Name}, No of kidney:{NumberOfKidney}, Health Status {HealthStatus}");
+    }
+
+
+
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp20;
+
+Person bernt = new Person("Berent", 2 , "need transplant");
+Person kåre = new Person("Kåre", 2 , "fit to donate kidney");
